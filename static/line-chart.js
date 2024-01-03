d7173e2436a1fb2a2d8fb85bfd97e95acba86df2
csaving <csaving78@att.net>
2024-01-02 21:57:15 -0600

    line chart test files

--- static/line-chart.js
+++ static/line-chart.js
@@ -3,16 +3,27 @@
 // function lineChart (metro) {
     // Get json data from url 
     // let url = "";
-    // d3.json(url).then(function(data) {
-        let home_values = [150007, 150146, 150410, 151068, 151860, 152748, 153693, 155027, 156593, 158398, 159766, 160885];
-        let sales_prices = [];
-        let months = ['2000-01-31', '2000-02-29', '2000-03-31', '2000-04-30', '2000-05-31', '2000-06-30', '2000-07-31', '2000-08-31', '2000-09-30', '2000-10-31', '2000-11-30', '2000-12-31'];
-  
-        let trace1 = {
-          x: months,
-          y: home_values,
-          type: 'scatter'
-        };
+
+    d3.csv("../Data/home_value_il.csv").then(function(data) {
+      console.log(data);  
+      
+      // let cities = []
+      // for (let i = 0; i < 5; i++) {
+        let city = data[0].RegionName;
+      //   cities.push(city)
+      // }
+      // console.log(cities)
+
+        let home_values = data[0]
+        console.log(home_values);
+        // let months = 
+
+
+        // let trace1 = {
+        //   x: months,
+        //   y: home_values,
+        //   type: 'scatter'
+        // };
           
         // let trace2 = {
         //     x: months,
@@ -20,8 +31,8 @@
         //     type: 'scatter'
         //   };
           
-        let data = [trace1];
+        // let data = [trace1];
           
-        Plotly.newPlot('line', data);
-    // })
+        // Plotly.newPlot('line', data);
+    })
 // }

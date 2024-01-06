7270695cdcebfc49189cb69ef14e7b5992f9b742
csaving <csaving78@att.net>
2024-01-05 21:47:25 -0600

    ap and line chart working from flask

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 # MongoDB Setup
 client = MongoClient('mongodb://localhost:27017/')
-db = client['chi_housing'] 
-suicides_collection = db['home_values'] 
+db = client['us_housing'] 
+hv = db['home_values'] 
 
 @app.route("/")
 def welcome():
@@ -15,45 +15,29 @@ def welcome():
 
 @app.route("/api/v1.0/barchart/<state_name>")
 def barchart(state_name): 
-     users = User.query.filter_by(state=StateName).all()
+    client = MongoClient(port=27017)
+    db = client['us_housing'] 
+    hv = db['home_values']  
 
-    if not users:
-        return jsonify({'message': 'No users found for this state abbreviation'}), 404
-    """"
-    given the state abbreviation, filter the data to return the city names and most recent years median house prices
-    
-    """
-    results = housing_values_collection.find({}, {"_id": 0, "RegionID": 1, "RegionName": 1, "StateName": 1, "2023-11-31": 1})
+    query = {"StateName": {"$exists": True, "$eq": state_name}}
+    projection = {"_id": False}
+
+    # Perform the find operation with field exclusion
+    results = hv.find(query, projection)
 
     all_values = []
+
     for record in results:
         all_values.append(record)
 
     return jsonify(all_values)
 
-@app.route("/api/v1.0/linechart/<state_name>")
-def linechart(state_name): 
-     users = User.query.filter_by(state=StateName).all()
+# @app.route("/api/v1.0/boundaries")
+# def boundary():
+#     with open("./static/data/allData_reduced.geojson") as file:
+#         json_decoded = json.load(file)
 
-    if not users:
-        return jsonify({'message': 'No users found for this state abbreviation'}), 404
-    """"
-    given the state abbreviation, return the 8 most recent years data for every city in the state 
-
-    """
-results = housing_values_collection.find({}, {"_id": 0, "RegionID": 1, "RegionName": 1, "StateName": 1, "2009-01-31": 1, "2010-01-31": 1, "2011-01-31" : 1, "2012-01-31" : 1, "2013-01-31" : 1, "2014-01-31" : 1, "2015-01-31" :1, "2016-01-31" : 1, "2017-01-31" : 1, "2018-01-31" : 1,"2019-01-31" : 1, "2020-01-31" :1, "2021-01-31" : 1, "2022-01-31" :1, "2023-01-31": 1})
-    all_values = []
-    for record in results:
-        all_values.append(record)
-
-    return jsonify(all_values)
-
-@app.route("/api/v1.0/boundaries")
-def boundary():
-    with open("./static/data/allData_reduced.geojson") as file:
-        json_decoded = json.load(file)
-
-    return json_decoded
+#     return json_decoded
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/logic.js
+++ static/logic.js
@@ -262,44 +262,29 @@ let myMap = L.map("map", {
     }
   ];
 
-
-  // Function to initialize data
+// Function to initialize data
 function init() {
+  // const url = `/api/v1.0/barchart/IL`;
+  // d3.json(url).then(data => {
+  //   console.log(data)
+  //   })
 
-    // Need to change dropdown to Map
-    // let dropdownMenu = d3.select("#selDataset");
+  // Add markers to the map
+  states.forEach(state => {
+    const newMarker = L.marker([state.latitude, state.longitude])
+      .bindPopup(`<h1>${state.state}</h1>`)
+      .addTo(myMap);
 
-    d3.csv('../clean_home_value.csv').then(data => {
-      // console.log(data)
-      })
-        // Looping through the states array, create one marker for each city, bind a popup containing its name and population, and add it to the map.
-        // for (let i = 0; i < states.length; i++) {
-        //   let state = states[i];
-        //   let marker = L.marker([state.latitude,state.longitude])
-        //   .bindPopup(`<h1>${state.state}</h1>`)
-        //    .addTo(myMap);
-        // }
+    // Attach the value to the marker for later access
+    newMarker.value = state.state;
+    // Add a click event listener to the marker
+    newMarker.on('click', onMarkerClick);
+    // Add a click event listener to the marker
+    newMarker.on('click', onMarkerClick);
+  });
 
-        // Add markers to the map
-          states.forEach(state => {
-            const newMarker = L.marker([state.latitude, state.longitude])
-              .bindPopup(`<h1>${state.state}</h1>`)
-              .addTo(myMap);
-
-            // Attach the value to the marker for later access
-            console.log(state.state)
-            newMarker.value = state.state;
-            // Add a click event listener to the marker
-            newMarker.on('click', onMarkerClick);
-            // Add a click event listener to the marker
-            newMarker.on('click', onMarkerClick);
-          });
-        
-    // let s = ['IL', 'IN', 'WI', 'CA', 'AK', 'WY', 'NY', 'AL']
-    // states.forEach(nameId => {dropdownMenu.append("option").text(nameId).property("value")})
-
-    // Call function to initialize charts
-    lineChart1('IL')
+  // Call function to initialize charts
+  lineChart1('IL')
 }; 
 
 // Function to handle marker click
@@ -308,85 +293,78 @@ function onMarkerClick(e) {
   const markerValue = e.target.value;
   console.log(markerValue)
   lineChart1(markerValue)
+  barChart(markerValue)
 }
 
-// // Function to update charts
-// function optionChanged(state) {
-//   lineChart1(state)
-//   // barChart(state)
-
-// }
-
 function lineChart1 (state) {
-  console.log(state)
-  // Get json data from url 
-  let url = "../Data/clean_home_value_test.csv";
+  // d3.csv('../clean_home_value.csv').then(data => {
 
-  d3.csv(url).then(function(data) {
+  // Get json data from url 
+  const url = `/api/v1.0/barchart/` + state;
+  console.log(url)
+  d3.json(url).then(function(data) {
     // console.log(data);  
 
-    let filterState = data.filter(regions => regions.StateName == state)
-    let sortedBySizeRank = filterState.sort((a, b) => a.SizeRank - b.SizeRank);
-    // console.log(sortedBySizeRank)
-    sliced = sortedBySizeRank.slice(0,8)
-    console.log(sliced)
-    // console.log(sliced.length)
+    // let filterState = data.filter(regions => regions.StateName == state)
+    let sorted = data.sort((a, b) => a.SizeRank - b.SizeRank);
+    let sliced = sorted.slice(0,8)
+    // console.log(sliced)
     let numCities = sliced.length
 
-      let months = ['2009-01', '2010-01', '2011-01', '2012-01', '2013-01', '2014-01', 
-                    '2015-01', '2016-01', '2017-01', '2018-01', '2019-01', '2020-01', 
-                    '2021-01', '2022-01', '2023-11']
-      let cities = []
-      let cityValues = []
-      for (let i = 0; i < numCities; i++) {
-        
-        let city = sliced[i].RegionName;
-        let homeValues = [Math.round(sliced[i].Jan2009), 
-                          Math.round(sliced[i].Jan2010),
-                          Math.round(sliced[i].Jan2011),
-                          Math.round(sliced[i].Jan2012),
-                          Math.round(sliced[i].Jan2013),
-                          Math.round(sliced[i].Jan2014),
-                          Math.round(sliced[i].Jan2015),
-                          Math.round(sliced[i].Jan2016),
-                          Math.round(sliced[i].Jan2017),
-                          Math.round(sliced[i].Jan2018),
-                          Math.round(sliced[i].Jan2019),
-                          Math.round(sliced[i].Jan2020),
-                          Math.round(sliced[i].Jan2021),
-                          Math.round(sliced[i].Jan2022),
-                          Math.round(sliced[i].Nov2023)
-                        ]
-        // console.log(city)
-        // console.log(homeValues)
-        cities.push(city)
-        cityValues.push(homeValues)
-      }
+    let months = ['2009-01', '2010-01', '2011-01', '2012-01', '2013-01', '2014-01', 
+                  '2015-01', '2016-01', '2017-01', '2018-01', '2019-01', '2020-01', 
+                  '2021-01', '2022-01', '2023-01', '2023-11']
+    let cities = []
+    let cityValues = []
+    for (let i = 0; i < numCities; i++) {      
+      let city = sliced[i].RegionName;
+      let homeValues = [Math.round(sliced[i]['2009-01-31']), 
+                        Math.round(sliced[i]['2010-01-31']),
+                        Math.round(sliced[i]['2011-01-31']),
+                        Math.round(sliced[i]['2012-01-31']),
+                        Math.round(sliced[i]['2013-01-31']),
+                        Math.round(sliced[i]['2014-01-31']),
+                        Math.round(sliced[i]['2015-01-31']),
+                        Math.round(sliced[i]['2016-01-31']),
+                        Math.round(sliced[i]['2017-01-31']),
+                        Math.round(sliced[i]['2018-01-31']),
+                        Math.round(sliced[i]['2019-01-31']),
+                        Math.round(sliced[i]['2020-01-31']),
+                        Math.round(sliced[i]['2021-01-31']),
+                        Math.round(sliced[i]['2022-01-31']),
+                        Math.round(sliced[i]['2023-01-31']),
+                        Math.round(sliced[i]['2023-11-30'])
+                      ]
+      console.log(city)
+      // console.log(homeValues)
+      cities.push(city)
+      cityValues.push(homeValues)
+    }
 
-      let lineData = []
-      for (let i = 0; i < numCities; i++) {
-        lineData.push({
-          x: months,
-          y: cityValues[i],
-          type: 'scatter',
-          name: cities[i]
-        });
-      }
-      // console.log(lineData)
+    let lineData = []
+    for (let i = 0; i < numCities; i++) {
+      lineData.push({
+        x: months,
+        y: cityValues[i],
+        type: 'scatter',
+        name: cities[i]
+      });
+    }
+    // console.log(lineData)
 
-      var layout = {
-        title: 'Mean Home Prices in ' + state,
-        xaxis: {
-          title: 'Date'
-        },
-        yaxis: {
-          title: 'Home Prices'
-          // range: [0, 1500000]
-        }
-      };  
- 
-      Plotly.newPlot('line1', lineData, layout);
-    })
+    var layout = {
+      title: 'Mean Home Prices in ' + state,
+      xaxis: {
+        title: 'Date'
+      },
+      yaxis: {
+        title: 'Home Prices'
+        // range: [0, 1500000]
+      }
+    };  
+
+    Plotly.newPlot('line1', lineData, layout);
+  })
 }
 
 // Add code for bar chart here
